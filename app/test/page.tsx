1fa052435ded129f81de36a0e69968cfb4bec5b3
Cam Burley <cam.burley@gmail.com>
2026-01-03 20:34:47 -0500

    Add test page to debug 404

---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+export default function TestPage() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'system-ui' }}>
+      <h1>Test Page Works!</h1>
+      <p>If you see this, routing is working.</p>
+    </div>
+  )
+}
+
